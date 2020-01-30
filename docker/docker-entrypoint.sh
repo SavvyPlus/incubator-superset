2a97c0742a38e357ebbf57e24a62174d81505086
Colin Wang <chenyangw233@gmail.com>
2020-01-30 17:47:42 +1100

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -17,11 +17,12 @@
 #
 set -eo pipefail
 
+export SUPERSET_MYSQL=${SUPERSET_MYSQL}
+
 if [ "${#}" -ne 0 ]; then
     exec "${@}"
 else
     gunicorn \
-        -e SUPERSET_MYSQL=${SUPERSET_MYSQL} \
         --bind  "0.0.0.0:${SUPERSET_PORT}" \
         --access-logfile '-' \
         --error-logfile '-' \
