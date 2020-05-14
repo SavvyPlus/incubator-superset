6ed6e4e1eb587840d1dc9962f1e8ad879fec5627
Colin Wang <chenyangw233@gmail.com>
2020-05-14 11:56:29 +1000

    Remove unnecessary imports

--- superset-frontend/src/generation/components/DataTable.jsx
+++ superset-frontend/src/generation/components/DataTable.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { lighten, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
 import EnhancedTableHead from './EnhancedTableHead';
 
 function createData(name, calories, fat, carbs, protein) {
@@ -67,10 +57,9 @@ const useStyles = makeStyles(theme => ({
 
 export default function DataTable() {
   const classes = useStyles();
-  const [selected, setSelected] = React.useState([]);
+  const [selected, setSelected] = React.useState(rows.map(n => n.name));
 
   const handleSelectAllClick = event => {
-    console.log('123');
     if (event.target.checked) {
       const newSelecteds = rows.map(n => n.name);
       setSelected(newSelecteds);
