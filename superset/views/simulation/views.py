0e53b85ce0c46eb13a2c0c7e7b33bb1c855930f9
Eric Dou <zhongfan.dou@zawee.work>
2020-07-14 18:13:35 +0800

    Add simulation status update for skip process

--- superset/views/simulation/views.py
+++ superset/views/simulation/views.py
@@ -1030,6 +1030,8 @@ class SimulationModelView(
                     message = 'Error sending to message to sqs, please try again later to contact dev team.'
                     g.detail = message
             else:
+                simulation.status = 'Running'
+                db.session.commit()
                 g.result = 'Skip pre process to invocation'
                 g.detail = 'Found existing assumption processed data, move to lambda invocation.'
                 simulation_start_invoker.apply_async(args=[simulation.run_id, 5])
