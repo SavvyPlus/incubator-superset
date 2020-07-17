a3f53d536de3a0fc35e52a35217aee6d60442e9b
Colin Wang <chenyangw233@gmail.com>
2020-07-17 18:40:58 +1000

    fix old charts issues

--- superset-frontend/src/explore/controlPanels/BoxPlotFin.js
+++ superset-frontend/src/explore/controlPanels/BoxPlotFin.js
@@ -17,6 +17,9 @@
  * under the License.
  */
 import { t } from '@superset-ui/translation';
+import { validateNonEmpty } from '@superset-ui/validator';
+import * as v from '../validators';
+import { formatSelectOptions } from '../../modules/utils';
 
 export default {
   controlPanelSections: [
@@ -27,13 +30,133 @@ export default {
         ['metrics'],
         ['adhoc_filters'],
         ['groupby'],
-        ['whisker_options'],
-        ['fin_scenario_picker'],
-        ['fin_firm_tech_picker'],
-        ['fin_period_picker'],
-        ['fin_metric_picker'],
-        ['fin_unit_picker'],
-        ['fin_tech_picker'],
+        [
+          {
+            name: 'whisker_options',
+            config: {
+              type: 'SelectControl',
+              freeForm: true,
+              label: t('Whisker/outlier options'),
+              default: 'Min/max (no outliers)',
+              description: t(
+                'Determines how whiskers and outliers are calculated.',
+              ),
+              choices: formatSelectOptions([
+                'Tukey',
+                'Min/max (no outliers)',
+                '2/98 percentiles',
+                '9/91 percentiles',
+              ]),
+            },
+          },
+        ],
+        [
+          {
+            name: 'fin_scenario_picker',
+            config: {
+              type: 'SelectControl',
+              multi: true,
+              label: t('Scenario'),
+              default: null,
+              description: t('Select one scenario'),
+              mapStateToProps: state => ({
+                choices: formatSelectOptions(state.fin_scenarios),
+              }),
+            },
+          },
+        ],
+        [
+          {
+            name: 'fin_firm_tech_picker',
+            config: {
+              type: 'SelectControl',
+              multi: false,
+              label: t('Firming Technology'),
+              default: null,
+              description: t('Select one firming technology'),
+              mapStateToProps: state => ({
+                choices: formatSelectOptions(state.fin_firm_techs),
+              }),
+            },
+          },
+        ],
+        [
+          {
+            name: 'fin_period_picker',
+            config: {
+              type: 'SelectControl',
+              multi: true,
+              label: t('Period'),
+              default: null,
+              description: t('Select one period'),
+              mapStateToProps: state => ({
+                choices: formatSelectOptions(state.fin_periods),
+              }),
+            },
+          },
+        ],
+        [
+          {
+            name: 'fin_metric_picker',
+            config: {
+              type: 'SelectControl',
+              multi: false,
+              label: t('Metric'),
+              default: 'PPA CFD',
+              validators: [validateNonEmpty],
+              description: t('Select metric'),
+              choices: [
+                ['PPA CFD', 'PPA CFD'],
+                ['MW Sold CFD', 'MW Sold CFD'],
+                [
+                  'Non-Firming Contribution Margin',
+                  'Non-Firming Contribution Margin',
+                ],
+                ['Contribution Margin', 'Contribution Margin'],
+                ['Fixed O&M', 'Fixed O&M'],
+                ['EBIT', 'EBIT'],
+                ['EBIT (Discounted)', 'EBIT (Discounted)'],
+                ['Capital Expenditure', 'Capital Expenditure'],
+                ['Terminal Value (Discounted)', 'Terminal Value (Discounted)'],
+                ['Net Present Value', 'Net Present Value'],
+                ['PPA CFD (Annually)', 'PPA CFD (Annually)'],
+                ['MW Sold CFD (Annually)', 'MW Sold CFD (Annually)'],
+              ],
+            },
+          },
+        ],
+        [
+          {
+            name: 'fin_unit_picker',
+            config: {
+              type: 'SelectControl',
+              multi: false,
+              label: t('Unit'),
+              default: '0',
+              validators: [validateNonEmpty],
+              description: t('Select unit'),
+              choices: [
+                ['0', '$'],
+                ['1', '$/MWh'],
+              ],
+            },
+          },
+        ],
+        [
+          {
+            name: 'fin_tech_picker',
+            config: {
+              type: 'SelectControl',
+              multi: true,
+              label: t('Technology'),
+              default: null,
+              description: t('Select multiple technologies'),
+              mapStateToProps: state => ({
+                choices: formatSelectOptions(state.fin_techs),
+              }),
+            },
+          },
+        ],
       ],
     },
     // {
