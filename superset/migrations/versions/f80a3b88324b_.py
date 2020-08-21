970ce218cfda01b15d9205c0b85104bce2764d32
Colin Wang <chenyangw233@gmail.com>
2020-08-21 14:34:18 +1000

    Update f80a3b88324b_.py

--- superset/migrations/versions/f80a3b88324b_.py
+++ superset/migrations/versions/f80a3b88324b_.py
@@ -24,7 +24,7 @@ Create Date: 2020-08-12 15:47:56.580191
 
 # revision identifiers, used by Alembic.
 revision = "f80a3b88324b"
-down_revision = ("978245563a02", "f120347acb39")
+down_revision = ("978245563a02", "f120347acb39", "fc472dad3d79")
 
 import sqlalchemy as sa
 from alembic import op
